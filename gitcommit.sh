825ed551157f7ac970fdfcecaefdfa7c164de76e
Gaurav Joshi <Gauravjsh0590@gmail.com>
2020-04-09 03:19:54 +0000

    2.txt modified

---
+++ gitcommit.sh
@@ -0,0 +1,19 @@
+#for commiting the changes
+#!/bin/bash
+
+echo "checking the git status"
+
+git status
+
+echo "adding all the modified files"
+
+git add .
+
+echo "commiting all the changes"
+
+echo "enter commit message"
+
+read message
+
+git commit -m "$message"
+
